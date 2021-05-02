f8dc08ffdbfba67dab6b1e2da6b1a4a2f5fab7d4
Tomek <t.wolyniec@tlen.pl>
2021-05-02 15:58:12 +0200

    order

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        PangramChecker.check("The quick brown fox jumps over the lazy dog.");
     }
 
 }
---
+++ src/Order.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Order {
+    public static String order(String words) {
+        String[] wordsTable = words.split(" ");
+        TreeMap<Integer, String> map = new TreeMap<>();
+        for (String w : wordsTable) {
+            int position = w.chars().filter(Character::isDigit).findFirst().orElse(0);
+            if (position != 0) map.put(position, w);
+        }
+        return String.join(" ", map.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Order.order("is2 Thi1s T4est 3a"));
+        System.out.println(Order.order("is This Test a"));
+
+    }
+}
